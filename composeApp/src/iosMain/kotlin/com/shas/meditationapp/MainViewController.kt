f78977ffe5bfa5cd9ac20c4e3a2a4f2866f8bb44
innov_muhammed.shijas <muhammed.shijas@innovaturelabs.com>
2026-01-20 13:27:16 +0530

    ios install issue

--- composeApp/src/iosMain/kotlin/com/shas/meditationapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/shas/meditationapp/MainViewController.kt
@@ -4,4 +4,4 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.shas.meditationapp.di.initKoin
 import com.shas.meditationapp.app.App
 
-fun MainViewController() = ComposeUIViewController(configure = initKoin()) { App() }
+fun MainViewController() = ComposeUIViewController(configure = { initKoin() }) { App() }
